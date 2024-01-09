208f3b553250091d89afaad1d2786e1fd26d0852
Ogordiunor Uche Jude <excelxz78@gmail.com>
2024-01-09 05:50:04 -0800

    third correction commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""Creates a class ``student``"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Defines a student object (based on ``9-student.py``"""
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """Initializes a student object.
+
+        Args:
+            first_name (str): student's first name.
+            last_name (str): student's lastname.
+            age (int): student's age.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.
+        """Returns the dictionary representation of ``Student`` object"""
+        if (attrs is None):
+            return (self.__dict__)
+
+        if ((type(attrs) != list) or
+                not all([type(item) is str for item in attrs])):
+            return
+
+        ret = {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return (ret)
