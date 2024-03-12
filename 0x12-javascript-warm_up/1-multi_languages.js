f478bcdbfc255fdc0ee71cb00ae510516974b52f
Ogordiunor Uche Jude <excelxz78@gmail.com>
2024-03-12 06:05:10 -0700

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const firstString = "C is fun";
+const secondString = "Python is cool";
+const thirString = "JavaScript is amazing";
+console.log(thirdString, secondString,thirdString);
